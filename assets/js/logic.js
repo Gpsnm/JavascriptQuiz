1dd5c38637d906681be29bc458f202980683523f
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-20 19:36:55 +0000

    added function to hide startscreen and start timer on click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,17 @@
 let startScreen = document.querySelector("#start-screen");
 let questionEl = document.querySelector("#questions");
-let startBtn = document.querySelector("start");
+let startBtn = document.querySelector("#start");
+let timeEl = document.querySelector("#time");
+let timerCount = 120;
+let timer;
+
+function startQuiz() {
+  startScreen.setAttribute("class", "hide");
+  questionEl.removeAttribute("class");
+  timer = setInterval(() => {
+    timeEl.textContent = timerCount;
+    timerCount--;
+  }, 1000);
+}
+
+startBtn.addEventListener("click", startQuiz);
