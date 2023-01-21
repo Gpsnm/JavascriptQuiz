6c2eaabbdc9e48aa6551a7f1e503ffe05fb1ecbc
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-21 14:03:03 +0000

    completed for loop inside getQuestion function that created 4 button elements and appends them too choices div

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,45 +4,35 @@ let startBtn = document.querySelector("#start");
 let timeEl = document.querySelector("#time");
 let questionTitle = document.querySelector("#question-title");
 let choices = document.querySelector("#choices");
-let choicesBtn1 = document.createElement("button");
-let choicesBtn2 = document.createElement("button");
-let choicesBtn3 = document.createElement("button");
-let choicesBtn4 = document.createElement("button");
 let timerCount = 120;
 let timer;
 
 
 
 // function that changes classes on start screen and question and also starts a timer.
-function startQuiz() {
-  startScreen.setAttribute("class", "hide");
-  questionEl.removeAttribute("class");
-  timer = setInterval(() => {
-    timeEl.textContent = timerCount;
-    timerCount--;
-  }, 1000);
-  getQuestions()
-}
+// function startQuiz() {
+//   startScreen.setAttribute("class", "hide");
+//   questionEl.removeAttribute("class");
+//   timer = setInterval(() => {
+//     timeEl.textContent = timerCount;
+//     timerCount--;
+//   }, 1000);
+//   getQuestions()
+// }
 
-startBtn.addEventListener("click", startQuiz);
+// startBtn.addEventListener("click", startQuiz);
 
 
 function getQuestions(){
 questionTitle.textContent = (questionsArr[0].questions);
 
-choices.appendChild(choicesBtn1);
-choicesBtn1.setAttribute("class", "choices");
-choicesBtn1.textContent = questionsArr[0].choice[0];
-choices.appendChild(choicesBtn2);
-choicesBtn2.setAttribute("class", "choices");
-choicesBtn2.textContent = questionsArr[0].choice[1];
-choices.appendChild(choicesBtn3);
-choicesBtn3.setAttribute("class", "choices");
-choicesBtn3.textContent = questionsArr[0].choice[2];
-choices.appendChild(choicesBtn4);
-choicesBtn4.setAttribute("class", "choices");
-choicesBtn4.textContent = questionsArr[0].choice[3];
+for (let i = 0; i < 4; i++){
+let choicesBtn = document.createElement("button");
+choices.appendChild(choicesBtn);
+choicesBtn.setAttribute("class", "choices");
+choicesBtn.textContent = questionsArr[0].choice[i];
+};
+};
 
 
-
-}
+getQuestions();
