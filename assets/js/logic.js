fa8d8f300336eff59bf5083248493b4e3059075e
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-22 17:10:27 +0000

    created a function that deducts 10 seconds from the running timer if the answer is incorrect

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,8 +8,6 @@ let timerCount = 120;
 let timer;
 let correctAnswer = questionsArr[0].answer;
 
-
-
 // function that changes classes on start screen and question and also starts a timer.
 function startQuiz() {
   startScreen.setAttribute("class", "hide");
@@ -27,17 +25,18 @@ function getQuestions() {
   questionTitle.textContent = questionsArr[0].questions;
 }
 
-function selectAnswer(){
-    for (let i = 0; i < 4; i++) {
+function selectAnswer() {
+  for (let i = 0; i < 4; i++) {
     let choicesBtn = document.createElement("button");
     choices.appendChild(choicesBtn);
     choicesBtn.setAttribute("class", "choices");
     choicesBtn.textContent = questionsArr[0].choice[i];
   }
-  }
-
   let answerButton = document.querySelector(".choices");
-  answerButton.value
-  answerButton.addEventListener("click", function(event){
-    console.log(event.value);
-  })
+  answerButton.addEventListener("click", function (event) {
+    let selectedButton = event.target.textContent;
+    if (selectedButton == questionsArr[0].answer) {
+      console.log("correct");
+    } else timerCount = timerCount - 10;
+  });
+}
